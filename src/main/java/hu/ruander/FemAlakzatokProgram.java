f41352786f81a95c7d525a32c4de23b4414840f0
dokibacsi <kvalcsi02@gmail.com>
2025-02-17 14:43:08 +0100

    femAlakzatokProgramban osszSuly + gombokOsszSuly

--- src/main/java/hu/ruander/FemAlakzatokProgram.java
+++ src/main/java/hu/ruander/FemAlakzatokProgram.java
@@ -13,8 +13,9 @@ public class FemAlakzatokProgram {
 
     public FemAlakzatokProgram () {
         this.femaalkzatok = new ArrayList<>(getLista());
+        ujAlakzat(new Hasab(3,1,2));
+        ujAlakzat(new Gomb(3));
     }
-
 
     public void ujAlakzat(Hasab hasab) {
         femaalkzatok.add(hasab);
@@ -26,5 +27,43 @@ public class FemAlakzatokProgram {
 
     public ArrayList<FemAlakzat> getLista(){
         return femaalkzatok;
+    }
+
+    public double osszSuly() {
+        double osszSuly = 0;
+        for (FemAlakzat femAlakzat : femaalkzatok) {
+            osszSuly += femAlakzat.suly();
+        }
+        return osszSuly;
+    }
+
+    public double gombokOsszSuly() {
+        double gombOsszSuly = 0;
+        for (FemAlakzat femAlakzat : femaalkzatok) {
+            if(femAlakzat.equals(gomb)){
+                gombOsszSuly += femAlakzat.suly();
+            }
+        }
+        return gombOsszSuly;
+    }
+
+    public FemAlakzat minV() {
+        double legkisebb = 0;
+        for (int i = 0; i < femaalkzatok.size(); i++) {
+            if(legkisebb < femaalkzatok.get(i).terfogat()){
+                legkisebb = femaalkzatok.get(i).terfogat();
+            }
+        }
+        return femaalkzatok.get((int) legkisebb);
+    }
+
+    public FemAlakzat maxV() {
+        double legnagyobb = 0;
+        for (int i = 0; i < femaalkzatok.size(); i++) {
+            if(legnagyobb < femaalkzatok.get(i).terfogat()){
+                legnagyobb = femaalkzatok.get(i).terfogat();
+            }
+        }
+        return femaalkzatok.get((int) legnagyobb);
     }
 }
--- src/test/java/hu/ruander/ProgramTest.java
+++ src/test/java/hu/ruander/ProgramTest.java
@@ -24,23 +24,23 @@ class ProgramTest {
         program.ujAlakzat(new Hasab(1,1,1));
     }
 
-    @Test
-    void testLista(){
-        ArrayList<FemAlakzat> lista = program.getLista();
-        Assertions.assertEquals(2, lista.size());
-
-        lista.add(new Gomb(1));
-        Assertions.assertEquals(2, program.getLista().size());
-    }
-//
 //    @Test
-//    void testSzamolasok(){
-//        Assertions.assertEquals(5.96, program.osszSuly(), 0.01);
-//        Assertions.assertEquals(4.81, program.gombokOsszSuly(), 0.01);
+//    void testLista(){
+//        ArrayList<FemAlakzat> lista = program.getLista();
+//        Assertions.assertEquals(2, lista.size());
 //
-//        Hasab hasab = new Hasab(1.0, 1.0, 1.0);
-//        Assertions.assertEquals(hasab.terfogat(), program.minV().terfogat());
-//        Gomb gomb = new Gomb(1.0);
-//        Assertions.assertEquals(gomb.terfogat(), program.maxV().terfogat());
+//        lista.add(new Gomb(1));
+//        Assertions.assertEquals(2, program.getLista().size());
 //    }
+
+    @Test
+    void testSzamolasok(){
+        Assertions.assertEquals(5.96, program.osszSuly(), 0.01);
+        Assertions.assertEquals(4.81, program.gombokOsszSuly(), 0.01);
+
+        Hasab hasab = new Hasab(1.0, 1.0, 1.0);
+        Assertions.assertEquals(hasab.terfogat(), program.minV().terfogat());
+        Gomb gomb = new Gomb(1.0);
+        Assertions.assertEquals(gomb.terfogat(), program.maxV().terfogat());
+    }
 }
